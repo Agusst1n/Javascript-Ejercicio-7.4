7bd10118205f8ad7923047786a36876cb6f2ede6
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2022-06-27 17:55:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//*! - Abre una nueva ventana "about:blank" en Google Chrome
+
+//*! - Abre la consola de desarrollador de Google Chrome (F12)
+
+//*! - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+
+
+let edad = parseInt(prompt("Cual es tu edad?"))
+//El codigo ya lo probe en el navegador y funciona correctamente, se guarda el valor en una variable
+//solo escribo el codigo aca para poder presentarlo
+
